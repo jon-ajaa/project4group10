2383357bba56947dcbe5afd49934fa492f3752a5
jon-ajaa <azhardevelop@gmail.com>
2024-12-31 15:04:54 +0700

    bit revision

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-// Fungsi untuk mengubah radio button yang terpilih
 function changeReview() {
     const radios = document.querySelectorAll('input[name="position"]');
     let currentPosition = 0;
     
-    // Temukan radio yang sedang terpilih
     radios.forEach((radio, index) => {
         if (radio.checked) {
             currentPosition = index;
         }
     });
 
-    // Pilih radio berikutnya (looping kembali ke yang pertama setelah yang terakhir)
     const nextPosition = (currentPosition + 1) % radios.length;
     radios[nextPosition].checked = true;
 }
 
-// Ubah posisi setiap 3 detik
 setInterval(changeReview, 3000);
